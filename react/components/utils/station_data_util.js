4224a30aadab1ebb1e975801e50dbd99dc6d4d69
JaredMcLellan <jaredamclellan@gmail.com>
2024-10-28 09:46:39 -0300

    Moved station data formatter to separate util file

---
+++ react/components/utils/station_data_util.js
@@ -0,0 +1,55 @@
+//Takes from ['erddap_data]['features']
+//Filter_station can be provided if you have the full feature data,
+//but only want one station
+export function station_data_format(features, filter_station) {
+    try {
+        let station_data = {}
+        const re_match = /(?<var_name>.*)\s\((?<standard_name>.*)\|(?<units>.*)\|(?<long_name>.*)\)/g; 
+        for (let feature in features){
+            const station_name = features[feature]['properties']['station']
+            // Can change the WFS query to only get one station, but for now easier to filter out here
+            if(!filter_station || filter_station==station_name){
+                const parsed_data = JSON.parse(features[feature]['properties']['station_data']);
+                if(station_data[station_name]) {
+                    station_data[station_name]['properties']['max_time'] = features[feature]['properties']['max_time']
+                }
+                else {
+                    station_data[station_name] = features[feature];
+                    let station_data_formatted = {
+                        'column_names':[],
+                        'column_units':[],
+                        'column_std_names':[],
+                        'column_long_names':[],
+                        'column_raw_names':[],
+                        'rows':[]
+                    };
+                    const data_fields = Object.keys(parsed_data[0]);
+
+                    data_fields.forEach((field) => {
+                        const names = [...field.matchAll(re_match)];
+                        if (names.length > 0) {
+                            station_data_formatted['column_names'].push(names[0].groups["var_name"]);
+                            station_data_formatted['column_std_names'].push(names[0].groups["standard_name"]);
+                            station_data_formatted['column_units'].push(names[0].groups["units"]);
+                            station_data_formatted['column_long_names'].push(names[0].groups["long_name"]);
+                            station_data_formatted['column_raw_names'].push(field);
+                        }
+                    })
+                    station_data[station_name]['properties']['station_data'] = station_data_formatted;
+                }
+                const station_column_data = station_data[station_name]['properties']['station_data']['column_raw_names']
+                parsed_data.forEach((row) => {
+                    let row_data = []
+                    station_column_data.forEach((column) => {
+                        row_data.push(row[column])
+                    })
+                    station_data[station_name]['properties']['station_data']['rows'].push(row_data)
+                })
+            }
+        }
+        return station_data
+    } catch (err) {
+        console.log("Error reformatting data")
+        return "oops"
+    }
+}
