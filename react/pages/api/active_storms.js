753577c9af7fcdd7bc3a1ad5681eab9027b96faa
sjbruce <scott.bruce@mi.mun.ca>
2024-12-10 10:34:30 -0330

    fixed bug with active storms

--- react/pages/api/active_storms.js
+++ react/pages/api/active_storms.js
@@ -6,11 +6,13 @@ export default async function handler(req, res) {
     const season = (req.query["season"]) ? req.query["season"] : new Date().getFullYear();
     const source = ["IBTRACS", "ECCC"];
     const source_type = "ACTIVE";
+    const storm_id = "";
+    const filters = {};
 
     console.log("handler", storm_name, season, source, source_type);
     
     try {
-        const result = await wfs_query(storm_name, season, source, source_type);
+        const result = await wfs_query(storm_name, season, source, source_type, storm_id, filters);
         res.status(200).json({ "storm_name": storm_name, "season": season, "source": source, "source_type": source_type, ...result });
     } catch (err) {
         res.status(500).json({ error: 'failed to load data', obj: err });
