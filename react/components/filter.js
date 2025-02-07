7c1b4959afd0ecff6a92474aeacf5bde8dae301a
Comfort Eboigbe <ceeboigbe@mun.ca>
2025-02-07 16:25:06 -0330

    Set up filter options

--- react/components/filter.js
+++ react/components/filter.js
@@ -3,54 +3,220 @@ import FilterListIcon from '@mui/icons-material/FilterList';
 import Search from "@mui/icons-material/Search";
 import { useEffect, useState } from 'react';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
+import { FitScreen } from "@mui/icons-material";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+//import Chip from '@mui/material/Chip';
 
+import Checkbox from '@mui/material/Checkbox';
+import Autocomplete from '@mui/material/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+
+const ITEM_HEIGHT = 35;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 150,
+    },
+  },
+};
 
 const filters = [
   { 
-    "filterName":'year',
-    "filterOption":[1996,1995,1998],
+    "name":'Year',
+    "option":[1996,1995,1998],
   },
   { 
-    "filterName":'storm category',
-    "filterOption":[1996,1995,1998],
+    "name":'Storm Category',
+    "option":["Cat 1","Cat 2","Cat 3"],
   },
   { 
-    "filterName":'Bounding box',
-    "filterOption":[1996,1995,1998],
+    "name":'Bounding Box',
+    "option":[1996,1995,1998],
   },
 
 
 ]
 
 export function RenderFilter(){
-  const [showfilterIcons, setShowfilterIcons] = useState(false); 
+  const [showFilterIcons, setShowfilterIcons] = useState(false); 
 
   function handleClick(){
-    setShowSearchForm((prev) => !prev); // Toggle form visibility
+    setShowfilterIcons((prev) => !prev); // Toggle form visibility
   };
 
   return(
-    <IconButton  aria-label='search'
-    id='filter-icon'
-    onClick={() => handleClick()}>
-      <FilterAltIcon
-      sx={{
-        fontSize: 'larger',
+    <>
+      {!showFilterIcons &&
+        (<IconButton  aria-label='search'
+      id='filter-icon'
+      onClick={() => handleClick()}>
+        <FilterAltIcon
+        sx={{
+          fontSize: 'larger',          
+        }}
+        />
+      </IconButton>)
+      }
+      {showFilterIcons && (
+        <FilterIcons
+        setShowfilterIcons = {setShowfilterIcons}
         
-        
-      }}
-      />
-    </IconButton>
+        />
+      )}
+    </>
   )
 }
 
-function FilterIcons(setShowfilterIcons){
-  <>
-    <Button
-      id="close-search-icon-small-screen"
+function FilterIcons({setShowfilterIcons}){
+  return(
+    <>
+    <Stack
+    direction="row"
+    spacing={0.5}
+    className='filter-icons-list'>
+      <Button
+      id="cancel-filter-icon"
+      className="filter-icons"
       onClick={() => {setShowfilterIcons(false)}}>
       X
     </Button> 
 
+      {
+        filters.map((filter, index) => {
+          return(
+            <>{/* 
+              <Chip icon={<FilterListIcon />} label={filter.name}
+              className="filter-icons"
+              /> */}
+              <MultipleSelectChip
+              filterName={filter.name}
+              options={filter.option}/>
+
+              <CheckboxesTags
+              filterName={filter.name}
+              options={filter.option}/>
+
+            </>
+
+            
+
+          )
+        })
+      }
+
+      {}
+      
+      
+
+
+
+    </Stack>
+    
+
+
   </>
+  )
+  
+}
+
+export  function MultipleSelectChip({filterName, options}) {
+  const [selectedOption, setSelectedOption] = useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedOption(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-chip-label">{filterName}</InputLabel>
+        <Select
+          labelId="demo-multiple-chip-label"
+          id="demo-multiple-chip"
+          multiple
+          value={selectedOption}
+          onChange={handleChange}
+          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+          renderValue={(selected) => (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+              {selected.map((value) => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+        >
+          {options.map((option) => (
+            <MenuItem
+              key={option}
+              value={option}
+              style={getStyles(option, selectedOption)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+function getStyles(option, selectedOption) {
+  return {
+    fontWeight: selectedOption.includes(option)
+      ? 'bold'
+      : 'normal',
+  };
+}
+
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+
+export function CheckboxesTags({filterName, options}) {
+  return (
+    <Autocomplete
+      sx={{paddingRight: '25px'}}
+  
+      multiple
+      id="checkboxes-tags-demo"
+      options={options}
+      disableCloseOnSelect
+      getOptionLabel={(options) => options}
+      renderOption={(props, options, { selected }) => {
+        const { key, ...optionProps } = props;
+        return (
+          <li key={key} {...optionProps}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {options}
+          </li>
+        );
+      }}
+      style={{ width: 200 }}
+      renderInput={(params) => (
+        <TextField {...params} label={filterName} />
+      )}
+    />
+  );
 }
