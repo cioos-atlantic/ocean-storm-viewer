feb0a12638ef201cb5b5e2e2f49bc80149983904
Comfort Eboigbe <ceeboigbe@mun.ca>
2025-04-23 15:19:35 -0230

    Create chart color list for combined chart

---
+++ react/components/Dashboard/Mobile_Dashboard/chart_color.js
@@ -0,0 +1,11 @@
+export const combined_graph_color = {
+  'Pressure':["#A26769", "#CEBEBE", "#D4A5A5", "#C7B198", "#A68A64", "#9D7B6F", "#BFA5A0", "#806D5A"]
+  ,
+  'Direction':['#A300A3'],
+  'Wave Height':["#FFADAD", "#FFD6A5", "#FDFFB6", "#CAFFBF", "#9BF6FF", "#A0C4FF", "#BDB2FF", "#FFC6FF"],
+  'Wind Speed':["#FF6B6B", "#4ECDC4", "#1A535C", "#FFA600", "#FF6361", "#58508D", "#003F5C", "#7A5195"]
+  ,
+  'Storm Speed':['#248F24'],
+  'Temperature':["#FF9F1C", "#FFBF69", "#CBF3F0", "#2EC4B6", "#E71D36", "#011627", "#FF3366", "#8338EC"],
+
+};
