3b73f8cb16ef15e29b904c191e9a9c32f7f69366
Comfort Eboigbe <ceeboigbe@mun.ca>
2025-05-07 16:20:05 -0230

    Remove unused files

--- react/components/Filter/boundingBox.js
+++ react/components/Filter/boundingBox.js
@@ -10,34 +10,6 @@ import 'leaflet-draw/dist/leaflet.draw.css';
 import 'leaflet-draw';
 
 
-export function RenderBoundingBox1(map){
-  let drawItems = new FeatureGroup();
-  map.addLayer(drawItems);
-
-
-  let drawControl = new Control.Draw({
-    edit:{
-      featureGroup: drawItems
-    },
-    draw:{
-      polygon: true,
-      polyline: true,
-      rectangle: true,
-      circle: true,
-      marker: true,
-    }
-  })
-
-  map.addControl(drawControl);
-   // Ensure drawn items are added to the layer
-   map.on(L.Draw.Event.CREATED, function (event) {
-    let layer = event.layer;
-    drawItems.addLayer(layer);
-  });
-};
-
-
-
 export function RenderBoundingBox(map){
   let drawItems = new FeatureGroup();
   map.addLayer(drawItems);
@@ -65,36 +37,6 @@ export function RenderBoundingBox(map){
 };
 
 
-export function RenderSpatialFilter({}){
 
-  return(
-    <Stack
-    direction="column"
-    
-    spacing={0.1}>
-      <IconButton  aria-label='search'className="spatial-filter-icon" sx={{top: "325px"}}
-        >
-      <RectangleOutlinedIcon
-      sx={{
-        fontSize: 'larger',
-        
-      }}
-      />
-      </IconButton>
-      <IconButton  aria-label='search' className="spatial-filter-icon"sx={{top: "375px"}}
-        >
-        <PentagonOutlinedIcon
-        sx={{
-          fontSize: 'larger',
-          
-        }}
-        />
-      </IconButton>
-
-    </Stack>
-    
-  )
-  
-}
 
 
