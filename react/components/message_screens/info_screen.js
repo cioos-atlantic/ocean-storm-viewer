1de7cace86a96b3bd740bae322a313a33db3c828
Jared McLellan <jaredamclellan@gmail.com>
2025-05-26 16:46:01 -0300

    Added some more filler to the dialog box

--- react/components/message_screens/info_screen.js
+++ react/components/message_screens/info_screen.js
@@ -1,13 +1,26 @@
-import { Dialog, DialogTitle } from "@mui/material";
+import { Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, } from "@mui/material";
 
 export default function InfoScreen({setInfo, open, onClose }){
     const handleClose = () => {
       setInfo(false)
     };
-    
+
     return(
-      <Dialog onClose = {handleClose} open={open}>
+      <Dialog onClose = {handleClose} open={open} PaperPropssx={{ align: 'center', display:'flex', justifyContent: 'center' }}>
         <DialogTitle>This is my help screen!</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+        <p>
+            Here is some information on using the site. 
+            Really this is just testing to see the functionality of a dialog box.
+            And also what it supports.
+            Like screenshots?
+        </p>
+        <img src="http://localhost:3500/ocean-storm-viewer/cioos-atlantic_EN.svg"></img>
+        <p>
+            I'm just a little curious how long we can make this section before the scroll bars come out
+        </p>
+        <img src="http://localhost:3500/ocean-storm-viewer/cioos-atlantic_EN.svg"></img>
+        </DialogContent>
       </Dialog>
     )
   
