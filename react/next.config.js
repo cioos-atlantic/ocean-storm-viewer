1f86a125c03f364a80218d29040ff2e2fe107b94
sjbruce <scott.bruce@mi.mun.ca>
2025-07-09 13:07:54 -0230

    added custom env variables to the process.env list of items, first item is the "base site title" this remains constant but page subtitles can be appended to it

--- react/next.config.js
+++ react/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   basePath: '/ocean-storm-viewer',
+  env: {
+    baseSiteTitle: 'Ocean Storm Viewer - ',
+  },
 }
 
 module.exports = nextConfig
