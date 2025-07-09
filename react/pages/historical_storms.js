070b28889814d28281a5977674c2e775c7683de3
sjbruce <scott.bruce@mi.mun.ca>
2025-07-09 14:04:39 -0230

    basic recreations of active and historical storm pages as individual pages rather than the all-in-one approach

---
+++ react/pages/historical_storms.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import PageHeader from './parts/page_header';
+import PageFooter from './parts/page_footer';
+import dynamic from 'next/dynamic';
+import { basePath } from '@/next.config';
+
+export default function HistoricalStormsPage() {
+    const [station_points, setStationPoints] = useState({});
+    
+    // useMemo() tells React to "memorize" the map component.
+    // Without this, the map will get redrawn by many interactions 
+    // and cause flashing - this lets us update map layers without
+    // the map constant flashing with each change and click.
+    const MapWithNoSSR = useMemo(
+        () => (dynamic(() => import("../components/map"), {
+            ssr: false
+        })),
+        [],
+    );
+
+    // useEffect(() => {
+    //     fetch(`${basePath}/api/query_stations`)
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setStationPoints(data);
+    //         })
+    // }, []);
+
+    return (
+        <>
+            <PageHeader
+                page_description={"Allows the user to search for historical storms and explore the recorded weather data from coastal stations and platforms during that time period."}
+                page_subtitle={"Historical Storms"}
+            />
+            <main className="body">
+
+                <MapWithNoSSR
+                    station_data={station_points}
+                    source_type={"historical"}
+                    setStationPoints={setStationPoints}
+                />
+
+            </main>
+            <PageFooter />
+        </>
+    );
+}
